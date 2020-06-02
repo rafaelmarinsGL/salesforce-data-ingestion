4386ac11fdc7890ad69f2b98eb4fea1350600562
rafaelCarinha <rcarinha@hotmail.com>
2020-06-02 09:01:29 -0600

    Fixing the build.

--- src/test/java/com/gorillalogic/salesforce/controller/IngestionControllerIntegrationTest.java
+++ src/test/java/com/gorillalogic/salesforce/controller/IngestionControllerIntegrationTest.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:application-dev.properties")
+@ActiveProfiles("test")
 public class IngestionControllerIntegrationTest {
 
     @Autowired
