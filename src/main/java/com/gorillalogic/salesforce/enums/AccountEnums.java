7be7009d34370a502bd45a450322ee0ab75ee98d
lgallardocr <luisrgardo@gmail.com>
2020-06-04 14:38:39 -0600

    LABS-69
    Add Models for: Contact, Candidate, Employee, Account and User

---
+++ src/main/java/com/gorillalogic/salesforce/enums/AccountEnums.java
@@ -0,0 +1,33 @@
+package com.gorillalogic.salesforce.enums;
+
+public class AccountEnums {
+
+    public enum CurrencyCode {
+
+    }
+
+    public enum Status {
+
+    }
+
+    public enum AccountSource {
+
+    }
+
+    public enum AccountRecordTye {
+
+    }
+
+    public enum Type {
+
+    }
+
+    public enum Industry {
+
+    }
+
+    public enum EmployeeRange {
+
+    }
+
+}
